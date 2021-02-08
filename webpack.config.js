b6bb8b1900e4e1a4bfa8695fbd3a9980e5e42840
shoghi-mor <shoghi-mor@tehran.ir>
2021-02-08 11:57:09 +0330

    edit sevrviceWorker js

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
-const sourcePath = path.join(__dirname, './src');
-const destinationPath = path.join(__dirname, './dist');
-
+const sourcePath = path.join(__dirname, "./src");
+const destinationPath = path.join(__dirname, "./dist");
 
 module.exports = {
-    devtool: 'inline-source-map',
+    devtool: "inline-source-map",
     context: sourcePath,
     // devtool: "eval",
     entry: [
@@ -17,72 +16,60 @@ module.exports = {
         "webpack/hot/only-dev-server",
         // active hot reload for react
         "react-hot-loader/patch",
-        "./index.jsx"
+        "./index.jsx",
     ],
     output: {
-        filename: 'bundle.js',
+        filename: "bundle.js",
         path: destinationPath,
         //for hot module replacement add bellow
-        publicPath: '/'
+        publicPath: "/",
     },
     module: {
         rules: [{
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: [
-                    "babel-loader"
-                ],
+                use: ["babel-loader"],
             },
             {
                 test: /\.css$/,
                 // exclude: /node_modules/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                ],
+                use: ["style-loader", "css-loader"],
             },
             {
                 test: /\.scss$/,
                 // exclude: /node_modules/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader',
-                ],
+                use: ["style-loader", "css-loader", "sass-loader"],
             },
             {
                 test: /\.less$/,
                 // exclude: /node_modules/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'less-loader',
-                ],
+                use: ["style-loader", "css-loader", "less-loader"],
             },
             {
                 test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-                loader: 'url-loader',
+                loader: "url-loader",
                 options: {
-                    limit: 10000
-                }
+                    limit: 10000,
+                },
             },
-
         ],
     },
     resolve: {
         alias: {
-            'react-dom': '@hot-loader/react-dom',
+            "react-dom": "@hot-loader/react-dom",
         },
-        extensions: ['.webpack-loader.js', '.web-loader.js', '.loader.js', '.js', '.jsx'],
-        modules: [
-            path.resolve(__dirname, 'node_modules'),
-            sourcePath
+        extensions: [
+            ".webpack-loader.js",
+            ".web-loader.js",
+            ".loader.js",
+            ".js",
+            ".jsx",
         ],
+        modules: [path.resolve(__dirname, "node_modules"), sourcePath],
     },
 
     // For Hot replacementadd brlow
     plugins: [
-
         //for log
         new webpack.NamedModulesPlugin(),
         //for detect changes
@@ -90,15 +77,18 @@ module.exports = {
         // new webpack.DefinePlugin({
         //     'process.env.NODE_ENV': JSON.stringify('development')
         // })
-       
     ],
     // For Use Dev server this config save output file in memory
     devServer: {
-        contentBase: './',
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+        },
+        contentBase: "./",
         port: 3030,
         hot: true,
         //same above public path for hot module replacement
-        publicPath: '/',
+        publicPath: "/",
         historyApiFallback: true,
-    }
+    },
 };
