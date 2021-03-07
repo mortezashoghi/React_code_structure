663ea981734bd17aafde2089a0682313ed29c5f4
shoghi-mor <shoghi-mor@tehran.ir>
2021-03-07 13:33:40 +0330

    edit todolist and routes

--- src/serviceworker.js
+++ src/serviceworker.js
@@ -1,10 +1,6 @@
 const sworeker = () => {
-  // console.log(`${process.env.PUBLIC_URL}`);
-  //`${process.env.PUBLIC_URL}
-  let sworkerUrl = "/serviceWker.js";
-  navigator.serviceWorker.register(sworkerUrl).then((responce) => {
-    // console.log("respnd");
-  });
+    //`${process.env.PUBLIC_URL}
+    let sworkerUrl = "/serviceWker.js";
+    navigator.serviceWorker.register(sworkerUrl).then((responce) => {});
 };
-
 export default sworeker;
